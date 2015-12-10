4908c546bbebdb6c3a0fd1728208642bbd03285e
nickkneafsey <nicholaskneafsey@gmail.com>
2015-12-09 19:38:59 -0800

    Dequeue is working

--- test/spec/SongQueueSpec.js
+++ test/spec/SongQueueSpec.js
@@ -19,17 +19,17 @@ describe('SongQueue', function() {
     SongQueue.prototype.playFirst.restore();
   });
 
-  describe('when a song is added', function() {
+  xdescribe('when a song is added', function() {
     describe('when it is the only song in the song queue', function() {
-      xit('plays it', function() {
+      it('plays it', function() {
         var songQueue = new SongQueue();
         songQueue.add(songData1);
         expect(playSpy).to.have.been.called;
       });
     });
 
-    describe('when it is not the only song in the song queue', function() {
-      xit('does nothing', function() {
+    xdescribe('when it is not the only song in the song queue', function() {
+      it('does nothing', function() {
         var songQueue = new SongQueue(songData1);
         songQueue.add(songData2);
         expect(playSpy).to.have.not.been.called;
@@ -37,8 +37,8 @@ describe('SongQueue', function() {
     });
   });
 
-  describe('when a song ends', function() {
-    xit('removes the song from the queue', function() {
+  xdescribe('when a song ends', function() {
+    it('removes the song from the queue', function() {
       var songQueue = new SongQueue([songData1, songData2]);
       song2 = songQueue.at(1);
       expect(songQueue.length).to.equal(2);
@@ -47,16 +47,16 @@ describe('SongQueue', function() {
       expect(songQueue.at(0)).to.equal(song2);
     });
 
-    describe('if there are any songs left in the queue', function() {
-    xit('plays the first song in the queue', function() {
+    xdescribe('if there are any songs left in the queue', function() {
+    it('plays the first song in the queue', function() {
         var songQueue = new SongQueue([songData1, songData2]);
         songQueue.at(0).ended();
         expect(playSpy).to.have.been.called;
       });
     });
 
-    describe('if there are no songs left in the queue', function() {
-      xit('does nothing', function() {
+    xdescribe('if there are no songs left in the queue', function() {
+      it('does nothing', function() {
         var songQueue = new SongQueue(songData1);
         songQueue.at(0).ended();
         expect(playSpy).to.have.not.been.called;
@@ -64,8 +64,8 @@ describe('SongQueue', function() {
     });
   });
 
-  describe('when a song is dequeued', function() {
-      xit('removes the song', function() {
+  xdescribe('when a song is dequeued', function() {
+      it('removes the song', function() {
       removeSpy = sinon.spy(SongQueue.prototype, 'remove');
       var songQueue = new SongQueue(songData1);
       songQueue.at(0).dequeue();
@@ -74,8 +74,8 @@ describe('SongQueue', function() {
     });
   });
 
-  describe('playFirst', function() {
-    xit('plays the first song in the queue', function() {
+  xdescribe('playFirst', function() {
+    it('plays the first song in the queue', function() {
       sinon.spy(SongModel.prototype, 'play');
       var songQueue = new SongQueue(songData1);
       songQueue.playFirst();
